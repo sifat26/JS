fe2f1861e60e67185673bd8f5df70975eae9faed
sifat26 <sifatict26@gmail.com>
2024-02-02 23:35:20 +0600

    JS practise

--- reverse string.js
+++ reverse string.js
@@ -1,3 +1,17 @@
-let sentence="Ami ekta valo chele";
-console.log(sentence);
+ let sentence="Ami ekta valo chele";
+ console.log(sentence);
+ let rev="";
+// for(let letter of sentence)
+// {
+//     rev=letter+rev;
+// }
 
+// console.log(rev);
+
+// for(i=0;i<sentence.length;i++)
+// {
+//     rev=sentence[i]+rev;
+// }
+// console.log(rev);
+let reverse=sentence.split('').reverse().join(' ');
+console.log(reverse);
